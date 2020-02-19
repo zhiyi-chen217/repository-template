8c86295eda67efb6a727f21bd2c1388602056de2
zhiyi chen <Z.Chen-29@student.tudelft.nl>
2020-02-19 16:16:43 +0100

    Modify userController

--- server/src/main/java/nl/tudelft/oopp/demo/controllers/UserController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/UserController.java
@@ -1,8 +1,5 @@
 package nl.tudelft.oopp.demo.controllers;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 import nl.tudelft.oopp.demo.entities.Quote;
 import nl.tudelft.oopp.demo.entities.Users;
 import nl.tudelft.oopp.demo.repositories.QuoteRepository;
@@ -13,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class QuoteController {
+public class UserController {
     /**
      * GET Endpoint to retrieve a random quote.
      *
@@ -27,30 +24,8 @@ public class QuoteController {
 
     @GetMapping("quote")
     @ResponseBody
-    public Users getRandomQuote() {
-        Quote q1 = new Quote(
-                1,
-                "A clever person solves a problem. A wise person avoids it.",
-                "Albert Einstein"
-        );
-
-        Quote q2 = new Quote(
-                2,
-                "The computer was born to solve problems that did not exist before.",
-                "Bill Gates"
-        );
-
-        Quote q3 = new Quote(
-                3,
-                "Tell me and I forget.  Teach me and I remember.  Involve me and I learn.",
-                "Benjamin Franklin"
-        );
-        quoteRepository.save(q1);
+    public Users getUser() {
         userRepository.save(new Users("admin","","12345") );
-        ArrayList<Quote> quotes = new ArrayList<>();
-        quotes.add(q1);
-        quotes.add(q2);
-        quotes.add(q3);
 
         return userRepository.findByUserId("admin");
     }
