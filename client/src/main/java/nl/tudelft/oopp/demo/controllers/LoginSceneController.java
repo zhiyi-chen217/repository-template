68304f63e12f8d02ae4a06317addaf35f9c5d9b3
Suleyman Ali <S.Ali-5@student.tudelft.nl>
2020-02-22 19:34:59 +0100

    added scene change after login

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/LoginSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/LoginSceneController.java
@@ -1,15 +1,17 @@
 package nl.tudelft.oopp.demo.controllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import java.io.IOException;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
-
 
 public class LoginSceneController {
     @FXML
@@ -19,24 +21,45 @@ public class LoginSceneController {
 
     /**
      * Submit the login credentials when clicked on submit.
+     * @param event the event that triggers this method
+     *
      */
-    public void submitClicked() {
+    public void submitClicked(ActionEvent event) throws IOException {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Submit clicked");
         alert.setHeaderText(null);
+
         String usertxt = username.getText();
         String pwtxt = password.getText();
-        alert.setContentText(ServerCommunication.sendLoginUser(usertxt, pwtxt));
-        alert.showAndWait();
+        String authentication = ServerCommunication.sendLoginUser(usertxt, pwtxt);
+
+        alert.setContentText(authentication);
+        alert.show();
+
+        //WIP: probably need to add a better form of authorization here.
+        if (authentication.contains(usertxt)) {
+            changeScene(event);
+        }
     }
 
     /**
      * When this method is called, the scene gets changed to the homepage.
+     * @param event the event that triggers this method
+     *
      */
+    public void changeScene(ActionEvent event) throws IOException {
+        Parent homePageParent = FXMLLoader.load(getClass().getResource("/homepageScene.fxml"));
+        Scene homePageScene = new Scene(homePageParent);
 
+        //Get current stage
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(homePageScene);
+        stage.show();
+    }
 
     /**
      * Submit the login credentials of an admin when clicked on submit.
+     * Note: this functionality will be removed soon (switching to single admin account).
      */
     public void adminClicked() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
