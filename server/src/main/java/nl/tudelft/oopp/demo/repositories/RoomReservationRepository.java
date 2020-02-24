7641e66a0579234ce72d41db91c78b284aedddf8
zhiyi chen <Z.Chen-29@student.tudelft.nl>
2020-02-24 13:20:19 +0100

    Configuration of checkstyle

--- server/src/main/java/nl/tudelft/oopp/demo/repositories/RoomReservationRepository.java
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/RoomReservationRepository.java
@@ -1,9 +1,10 @@
 package nl.tudelft.oopp.demo.repositories;
 
 
+import nl.tudelft.oopp.demo.entities.RoomReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import nl.tudelft.oopp.demo.entities.RoomReservation;
+
 
 @Repository
 public interface RoomReservationRepository extends JpaRepository<RoomReservation, Long> {
