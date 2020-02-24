878b7c7444f55083d287bbb849e5eb14bb9f20f8
Luciano Carlo Hagemans <l.c.hagemans@student.tudelft.nl>
2020-02-24 19:14:53 +0100

    Added signup page without server communication

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/HomepageController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/HomepageController.java
@@ -1,14 +1,22 @@
 package nl.tudelft.oopp.demo.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import nl.tudelft.oopp.demo.communication.ServerCommunication;
 
 public class HomepageController {
     @FXML
     private Label welcomeLabel;
+    @FXML
+    private Button logoutBtn;
 
     public void setWelcomeMessage(String message) {
         welcomeLabel.setText("Welcome " + message);
+    }
+
+    public void logout() {
+        ServerCommunication.resetPubAuth();
     }
 }
 
---
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/SignUpController.java
@@ -0,0 +1,80 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import nl.tudelft.oopp.demo.communication.ServerCommunication;
+
+import java.io.IOException;
+
+
+public class SignUpController {
+    @FXML
+    private TextField netid;
+    @FXML
+    private TextField email1;
+    @FXML
+    private TextField email2;
+    @FXML
+    private PasswordField pass1;
+    @FXML
+    private PasswordField pass2;
+    @FXML
+    private Label failtext;
+
+    /**
+     * Send the sign up details to the server.
+     */
+    public void signUp(ActionEvent event) {
+        failtext.setText("");
+        String netidstr = netid.getText();
+        String emailstr1 = email1.getText();
+        String emailstr2 = email2.getText();
+        String passstr1 = pass1.getText();
+        String passstr2 = pass2.getText();
+
+        if (netidstr.length() == 0) {
+            failtext.setText("Please insert a valid netid.");
+            return;
+        }
+        if (emailstr1.length() == 0) {
+            failtext.setText("Please insert a valid email address.");
+            return;
+        }
+        if (!emailstr1.equals(emailstr2)) {
+            failtext.setText("Please make sure the two email addresses are the same..");
+            return;
+        }
+        if (passstr1.length() == 0) {
+            failtext.setText("Please insert a valid password.");
+            return;
+        }
+        if (!passstr1.equals(passstr2)) {
+            failtext.setText("Please make sure the two passwords match.");
+            return;
+        }
+
+        ServerCommunication.sendSignUp(netidstr, emailstr1, passstr1);
+        
+        try {
+            Parent signupPageParent = FXMLLoader.load(getClass().getResource("/loginScene.fxml"));
+            Scene signupPageScene = new Scene(signupPageParent);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(signupPageScene);
+            stage.show();
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("An error occurred, please try again.");
+        }
+
+
+    }
+}
