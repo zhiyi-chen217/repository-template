24e49268aa75edd4a3a27c481f4b21ff54924e00
Suleyman Ali <S.Ali-5@student.tudelft.nl>
2020-02-24 11:03:47 +0100

    added ServerCommunication test

--- client/src/test/java/nl/tudelft/oopp/demo/communication/test/ServerCommunicationTest.java
+++ client/src/test/java/nl/tudelft/oopp/demo/communication/test/ServerCommunicationTest.java
@@ -9,7 +9,14 @@ import org.junit.jupiter.api.Test;
 
 public class ServerCommunicationTest {
     @Test
-    public void testLogIn(){
+    public void sendLoginTest(){
         assertNotNull(ServerCommunication.sendLogin("admin", "12345","http://localhost:8080/login"));
     }
+
+    @Test
+    public void sendLoginUserTest(){
+        assertEquals("Hello admin", ServerCommunication.sendLoginUser("admin", "12345"));
+    }
+
+
 }
