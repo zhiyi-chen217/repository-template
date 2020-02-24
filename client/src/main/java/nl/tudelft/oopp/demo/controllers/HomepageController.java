52cbf58b6ee56112ca99480c9010f7ff9bc572bc
Luciano Carlo Hagemans <l.c.hagemans@student.tudelft.nl>
2020-02-24 22:02:50 +0100

    Improved the logout procedure

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/HomepageController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/HomepageController.java
@@ -1,9 +1,17 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
+
+import java.io.IOException;
 
 public class HomepageController {
     @FXML
@@ -15,8 +23,22 @@ public class HomepageController {
         welcomeLabel.setText("Welcome " + message);
     }
 
-    public void logout() {
+    /**This function makes sure the user is logged out once the logout button is pressed.
+     *
+     * @param event the event which causes this logout function to start
+     * @throws IOException the exception can be thrown if the fxml loader cant load the resource
+     */
+    public void logout(ActionEvent event) throws IOException {
         ServerCommunication.resetPubAuth();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/mainScene.fxml"));
+        Parent homePageParent = loader.load();
+        Scene homePageScene = new Scene(homePageParent);
+
+        //Get current stage
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(homePageScene);
+        stage.show();
     }
 }
 
