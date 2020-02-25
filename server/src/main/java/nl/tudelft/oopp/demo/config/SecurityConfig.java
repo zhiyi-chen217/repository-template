0e577a6aa3ea1047a84aa60f79031033f54a5b17
zhiyi chen <Z.Chen-29@student.tudelft.nl>
2020-02-25 02:14:54 +0100

    save new buildings

--- server/src/main/java/nl/tudelft/oopp/demo/config/SecurityConfig.java
+++ server/src/main/java/nl/tudelft/oopp/demo/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/**")
                 .authenticated()
--- server/src/main/java/nl/tudelft/oopp/demo/controllers/BuildingController.java
+++ server/src/main/java/nl/tudelft/oopp/demo/controllers/BuildingController.java
@@ -1,8 +1,30 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import nl.tudelft.oopp.demo.entities.Building;
+import nl.tudelft.oopp.demo.repositories.BuildingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @RestController
 public class BuildingController {
 
+    @Autowired
+    private BuildingRepository buildingRepository;
+
+    @PostMapping("buildings")
+    public String newBuilding(@RequestBody Building building, HttpServletResponse httpResponse) {
+        Optional<Building> buildingOptional =  buildingRepository.findByName(building.getName());
+        if(buildingOptional.isPresent()){
+            httpResponse.setStatus(400);
+            return "building already exists";
+        }
+        httpResponse.setStatus(201);
+        buildingRepository.save(building);
+        return "Saved successfully";
+    }
 }
