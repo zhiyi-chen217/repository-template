18613a004b528fc2f3767140ab6bcdba02f426c0
Zhiqiang LEI <z.lei-1@student.tudelft.nl>
2020-03-05 21:32:08 +0100

    Create order information

---
+++ server/src/main/java/nl/tudelft/oopp/demo/entities/OrderInformation.java
@@ -0,0 +1,9 @@
+package nl.tudelft.oopp.demo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+// @Entity
+// @Table(name = "orderInformation")
+public class OrderInformation {
+}
